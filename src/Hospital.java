bf1e0def40cdee20b09067a232a09ff195bb02e0
Yildiz Sen <yildizsen93@gmail.com>
2022-11-29 20:46:57 -0600

    hospital practice project

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,6 +1,37 @@
-public class Hospital {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
+    public class Hospital {
 
+        public static void main(String[] args) {
+
+            /**
+             * 1. Create 3 patients
+             * 2. Assign 3 doctors according to their problems
+             * 3. Print out the patient
+             * 4. Print out the doctor of the patient
+             */
+            System.out.println(QuestionsAndMessages.welcomeMessage);
+
+            List<Patient> patients = new ArrayList<>();
+
+            for (int i = 0; i < 3; i++) {
+                Patient currentPatient = Patient.createPatient();
+
+                patients.add(currentPatient);
+                patients.get(i).patientDoctor = Doctor.getDoctor(patients.get(i).problem); // gets current patient using get(i). data type is Doctor = method(patient)
+                patients.get(i).patientDoctor.isAvailable = false;
+                System.out.println(QuestionsAndMessages.doctorMessage);
+            }
+
+            for (Patient patient : patients) {
+                System.out.println(patient);
+                System.out.println(patient.patientDoctor);
+                System.out.println(QuestionsAndMessages.goodByeMessage);
+            }
+
+
+
+        }
     }
-}
+
--- src/QuestionsAndMessages.java
+++ src/QuestionsAndMessages.java
@@ -1,4 +1,11 @@
 public class QuestionsAndMessages {
 
+    public static final String welcomeMessage = "Welcome to TechGlobal hospital!\n";
+    public static final String doctorMessage = "Your doctor has been assigned";
+    public static final String noInsuranceMessage = "You dont have insruance, here is your bill = " + Integer.MIN_VALUE;
+    public static final String emergencyMessage = "You will get you treatment ASAP!";
+    public static final String goodByeMessage = "Thank you for choosing us!";
+
+
 
 }
